3ec6b9de1c31dd17fefcf0261b70fa97b6c43843
Evans <Shoowa@users.noreply.github.com>
2016-01-16 13:07:36 +0800

    Created a looping ring for messages.

---
+++ ring3.erl
@@ -0,0 +1,25 @@
+-module(ring3).
+-compile(export_all).
+
+nodeMessenger() ->
+	receive
+		{info, [Next|RemainingPIDs], NodeList, M, Message} when M > 0 ->
+			io:format("I am ~w and this is the message: ~w~n", [self(), Message]),
+			Next ! {info, RemainingPIDs, NodeList, M-1, Message},
+			nodeMessenger();
+		{info, [], NodeList, M, Message} when M > 0 ->
+			io:format("Last node: ~w, and the message: ~w~n", [self(), Message]),
+			hd(NodeList) ! {info, tl(NodeList), NodeList, M-1, Message},
+			nodeMessenger();
+		{info, _, _, 0, _} ->
+			io:format("The end.~n")
+	end.
+
+start(M, N, Message) ->
+	process_builder(M, N, Message, []).
+
+process_builder(M, N, Message, NodeList) when N > 0 ->
+	PID = spawn(ring3, nodeMessenger, []),
+	process_builder(M, N-1, Message, [PID|NodeList]);
+process_builder(M, 0, Message, NodeList) ->
+	hd(NodeList) ! {info, tl(NodeList), NodeList, M, Message}.
