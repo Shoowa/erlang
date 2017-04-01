2666d33e4aeb58e24460c485370cacd3a9c21c32
Evans <Shoowa@users.noreply.github.com>
2017-04-01 11:19:40 -0400

    add comments, specs, and column functions.

--- sudoku.erl
+++ sudoku.erl
@@ -1,7 +1,9 @@
 -module(sudoku).
--export([fetch_good_sudoku/0, fetch_bad_sudoku/0, sum45/1, create_column/1, create_tail_grid/1]).
+-export([fetch_good_sudoku/0, fetch_bad_sudoku/0, sum45/1, create_column/1, create_tail_grid/1, nine_numbers/1, check_columns/1]).
 
-%% For quick & dirty purposes, a Sudoku grid is represented as a list of lists.
+-type grid() :: list(list(integer())).
+
+%% For quick & dirty purposes, a Sudoku grid is represented as a list of listed integers. No functions check the size of a grid. The aforementioned type definition of 'grid()' matches this specification.
 fetch_good_sudoku() ->
          [
             [3, 9, 1, 2, 8, 6, 5, 7, 4],
@@ -28,14 +30,48 @@ fetch_bad_sudoku() ->
             [6, 5, 7, 3, 2, 1, 9, 8, 6]
         ].
 
--spec sum45(Line::list(integer())) -> boolean().
+%% Recursively inspect a list for digits 1 through 9. Return a TRUE or FALSE value.
+-spec nine_numbers(Line :: list(integer())) -> boolean().
+nine_numbers(Line) ->
+    nine_numbers(9, Line).
+
+-spec nine_numbers(N :: integer(), Line :: list()) -> boolean().
+nine_numbers(0, Line) ->
+    true;
+nine_numbers(N, Line) when N > 0 ->
+    case lists:member(N, Line) of
+        true ->
+            nine_numbers(N-1, Line);
+        false ->
+            io:format("Failure! The list lacks nine unique digits~n"),
+            false
+    end.
+
+%% The sum of 1 through 9 is 45. Return TRUE or FALSE if a list, ideally composed of digits 1 through 9, adds up to 45.
+-spec sum45(Line :: list(integer())) -> boolean().
 sum45(Line) ->
     lists:sum(Line) =:= 45.
 
--spec create_column(Grid::list()) -> list(integer()).
+%% Peels the head off each list in a Sudoku grid composed of nine lists, then produces a new list from those nine heads.
+-spec create_column(Grid :: grid()) -> list(integer()).
 create_column(Grid) ->
     [ hd(X) || X <- Grid ].
 
--spec create_tail_grid(Grid::list()) -> list(list()).
+%% Peels the heads of nine lists off, then returns the tails of all nine lists.
+-spec create_tail_grid(Grid :: grid()) -> list(list()).
 create_tail_grid(Grid) ->
-    [ tl(X) || X <- Grid ].
+    [ tl(X) || X <- Grid ]. 
+
+-spec check_columns(Grid :: grid()) -> boolean().
+check_columns([[],[],[],[],[],[],[],[],[]]) ->
+    true;
+check_columns(Grid) ->
+    Column = create_column(Grid),
+    case nine_numbers(Column) of
+        true ->
+            LesserGrid = create_tail_grid(Grid),
+            check_columns(LesserGrid);
+        false ->
+            io:format("Failure! The column lacks nine unique digits.~n"),
+            false
+    end.
